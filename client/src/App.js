87fc4381eb4ad7d923ea340bff13ffaed7814c6a
Riza Khan <khanriza@gmail.com>
2019-09-21 00:13:49 -0400

    first big push

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return <div className="title">Blog App by <h1>---Otonye Iyalla---</h1> and <h1>---Riza Khan---</h1></div>
+  }
+
+
+
+}
+
+
+export default App;
